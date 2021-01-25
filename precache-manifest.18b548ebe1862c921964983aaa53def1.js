f1bceab10e829800030501193a97fc206f96d8b8
Campana Damian <damian@aridosoftware.com>
2021-01-25 16:44:58 -0300

    Updates

---
+++ precache-manifest.18b548ebe1862c921964983aaa53def1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5754ee0dce0c42017354148a4876de5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72307fb565b378d629bc",
+    "url": "./static/css/main.b9cdb34b.chunk.css"
+  },
+  {
+    "revision": "7f3b084fbeee14e54b67",
+    "url": "./static/js/2.0a0b304b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0a0b304b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72307fb565b378d629bc",
+    "url": "./static/js/main.2ff37e4f.chunk.js"
+  },
+  {
+    "revision": "29c0f3f28096868927c4",
+    "url": "./static/js/runtime-main.e523d792.js"
+  }
+]);
