9fb03b24205b0e1fbb8606d94ad5cb2d47affa4c
Campana Damian <damian@aridosoftware.com>
2021-01-26 15:19:42 -0300

    Updates

---
+++ precache-manifest.940677d27e1413ac4064e51cfe73497b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "551ff89a336a5c950a3f96bdd42aa7d9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "80a751aeae487672bd04",
+    "url": "./static/css/main.090aced5.chunk.css"
+  },
+  {
+    "revision": "6ed92d3b4de8d9d89e39",
+    "url": "./static/js/2.eab94b20.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.eab94b20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80a751aeae487672bd04",
+    "url": "./static/js/main.06d780eb.chunk.js"
+  },
+  {
+    "revision": "29c0f3f28096868927c4",
+    "url": "./static/js/runtime-main.e523d792.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9c4bf5541e01217cb9728d396cc2d1c9.js"
+  "./precache-manifest.940677d27e1413ac4064e51cfe73497b.js"
 );
 
 self.addEventListener('message', (event) => {
